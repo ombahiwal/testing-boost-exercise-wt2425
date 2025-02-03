f45bf1e4c67c54ef00e7eb670dc0dd9f96010767
ombahiwal <ombahiwal@gmail.com>
2025-02-03 03:44:19 +0100

    fix test code

--- tests/matrixIOTest.cpp
+++ tests/matrixIOTest.cpp
@@ -1,20 +1,18 @@
 #define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
 #include "matrixIO.hpp"
-
-using Eigen::MatrixXd;
+#include <Eigen/Dense>
 
 BOOST_AUTO_TEST_SUITE(MatrixIOTests)
 
 BOOST_AUTO_TEST_CASE(TestOpenData_ValidFile)
 {
   // Arrange
-  const std::string validFilePath = "../data/matrix.csv"; // Ensure this file exists with valid data
+  const std::string validFilePath = "../data/matrix.csv"; // Ensure this file exists
   const int         matrixSize    = 3;
-  matrixIO::MatrixIO matrixIO;
 
   // Act
-  MatrixXd resultMatrix = matrixIO.openData(validFilePath, matrixSize);
+  Eigen::MatrixXd resultMatrix = matrixIO::openData(validFilePath, matrixSize);
 
   // Assert
   BOOST_TEST(resultMatrix.rows() == matrixSize);
